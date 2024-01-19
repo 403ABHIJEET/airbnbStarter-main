1456063dd8ffa2e34b0da9dfb78751759ac347ff
Abhijeet Kumar <133308751+403ABHIJEET@users.noreply.github.com>
2024-01-19 12:00:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+import { list, list2 } from "./assets/cards-list";
+
+function App() {
+  const [selectedFilter, setSelectedFilter] = useState(0);
+  
+}
+
+export default App;
